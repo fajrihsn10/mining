47a8ad1e7ec538d081b43dbf3c383121935ee14e
fajrihsn10 <fajrihsn10@gmail.com>
2024-12-04 14:08:46 +0700

    Update start-mining.sh

--- start-mining.sh
+++ start-mining.sh
@@ -3,4 +3,4 @@
 cd /SRBMiner-Multi-2-7-1
 
 # Perintah untuk memulai mining
-./SRBMiner-MULTI --algorithm verushash --pool ap.luckpool.net:3956 --wallet RNB3BZ2zvr8F3qhjYbktEQ1xGosgiCPdwv --Worker fajri --password x --cpu-threads 0
+./SRBMiner-MULTI --algorithm verushash --pool na.luckpool.net:3956 --wallet RNB3BZ2zvr8F3qhjYbktEQ1xGosgiCPdwv --Worker fajri --password x --cpu-threads 0
